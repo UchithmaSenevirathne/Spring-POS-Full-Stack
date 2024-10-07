b85cbd96f408dd5ba3160cfe3e9daed6e7e0fd6c
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-07 15:10:32 +0530

    spring data jpa repositories using javaconfig

--- POS-Backend/src/main/java/lk/ijse/posbackend/config/WebAppRootConfig.java
+++ POS-Backend/src/main/java/lk/ijse/posbackend/config/WebAppRootConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.posbackend")
@@ -16,5 +19,15 @@ public class WebAppRootConfig {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        var dbms = new DriverManagerDataSource();
+        dbms.setDriverClassName("com.mysql.jdbc.Driver");
+        dbms.setUrl("jdbc:mysql://localhost:3306/pos_javaee?createDatabaseIfNotExist=true");
+        dbms.setUsername("root");
+        dbms.setPassword("1234");
+        return dbms;
     }
 }
