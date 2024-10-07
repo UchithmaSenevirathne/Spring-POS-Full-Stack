41e7d495d2f054b8dd48d475a1851b1339e24f3a
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-07 15:13:24 +0530

    create bean for transactional usages

--- POS-Backend/src/main/java/lk/ijse/posbackend/config/WebAppRootConfig.java
+++ POS-Backend/src/main/java/lk/ijse/posbackend/config/WebAppRootConfig.java
@@ -1,13 +1,16 @@
 package lk.ijse.posbackend.config;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -43,5 +46,12 @@ public class WebAppRootConfig {
         factory.setPackagesToScan("lk.ijse.posbackend.entity");
         factory.setDataSource(dataSource());
         return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
     }
 }
