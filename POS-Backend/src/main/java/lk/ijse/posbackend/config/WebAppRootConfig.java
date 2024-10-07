bbde9a5b2891aac6c81475a7697ac9b9ea4f7fbb
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-07 15:07:41 +0530

    add model mapper bean

--- POS-Backend/src/main/java/lk/ijse/posbackend/config/WebAppRootConfig.java
+++ POS-Backend/src/main/java/lk/ijse/posbackend/config/WebAppRootConfig.java
@@ -1,5 +1,7 @@
 package lk.ijse.posbackend.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,4 +12,9 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories(basePackages = "lk.ijse.posbackend")
 @EnableTransactionManagement
 public class WebAppRootConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
