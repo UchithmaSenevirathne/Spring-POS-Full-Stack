7a445dd5f7d3f52339643fd39e9386874f6c63d1
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-07 16:33:23 +0530

    create place order service

---
+++ POS-Backend/src/main/java/lk/ijse/posbackend/service/PlaceOrderService.java
@@ -0,0 +1,4 @@
+package lk.ijse.posbackend.service;
+
+public interface PlaceOrderService {
+}
---
+++ POS-Backend/src/main/java/lk/ijse/posbackend/service/PlaceOrderServiceImpl.java
@@ -0,0 +1,9 @@
+package lk.ijse.posbackend.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class PlaceOrderServiceImpl implements PlaceOrderService {
+}
