ff365e2d2087efad8d42556f08d318612fa74a61
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-07 16:31:07 +0530

    create customer service

--- POS-Backend/src/main/java/lk/ijse/posbackend/controller/Customer.java
+++ POS-Backend/src/main/java/lk/ijse/posbackend/controller/Customer.java
@@ -1,4 +1,14 @@
 package lk.ijse.posbackend.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customer")
+@RequiredArgsConstructor
 public class Customer {
+
+
 }
