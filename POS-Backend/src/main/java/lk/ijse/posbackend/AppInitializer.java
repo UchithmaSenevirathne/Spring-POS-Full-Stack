3b823e64e74d185bf2fcf6d3c8e9e1f9bbc4c55c
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-07 15:01:39 +0530

    create folders

--- POS-Backend/src/main/java/lk/ijse/posbackend/AppInitializer.java
+++ POS-Backend/src/main/java/lk/ijse/posbackend/AppInitializer.java
@@ -5,7 +5,7 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
-public class HelloServlet extends HttpServlet {
+public class AppInitializer extends HttpServlet {
     private String message;
 
     public void init() {
