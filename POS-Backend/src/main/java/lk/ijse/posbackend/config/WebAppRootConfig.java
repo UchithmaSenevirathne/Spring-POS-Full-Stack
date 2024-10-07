d0adf4be13c3909dbafac9f4b3d7dcc5e520813d
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-07 15:11:56 +0530

    create entity manager

--- POS-Backend/src/main/java/lk/ijse/posbackend/config/WebAppRootConfig.java
+++ POS-Backend/src/main/java/lk/ijse/posbackend/config/WebAppRootConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -29,5 +31,17 @@ public class WebAppRootConfig {
         dbms.setUsername("root");
         dbms.setPassword("1234");
         return dbms;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("lk.ijse.posbackend.entity");
+        factory.setDataSource(dataSource());
+        return factory;
     }
 }
