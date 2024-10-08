14b93f30bc43ed6572f8463df3a98596edbe8128
UchithmaSenevirathne <uchithma@gmail.com>
2024-10-08 09:50:01 +0530

    update code

--- POS-Backend/src/main/java/lk/ijse/posbackend/controller/PlaceOrder.java
+++ POS-Backend/src/main/java/lk/ijse/posbackend/controller/PlaceOrder.java
@@ -5,6 +5,8 @@ import lk.ijse.posbackend.dto.OrderDetailsDTO;
 import lk.ijse.posbackend.exception.DataPersistFailedException;
 import lk.ijse.posbackend.service.PlaceOrderService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,6 +23,8 @@ public class PlaceOrder {
     @Autowired
     private final PlaceOrderService placeOrderService;
 
+    private static final Logger logger = LoggerFactory.getLogger(PlaceOrder.class);
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> placeOrder(@RequestBody OrderDTO orderDTO) {
         if (orderDTO == null){
@@ -28,11 +32,13 @@ public class PlaceOrder {
         }else {
             try {
                 placeOrderService.saveOrder(orderDTO);
+                logger.info("Order placed successfully: {}", orderDTO);
                 return new ResponseEntity<>(HttpStatus.CREATED);
             }catch (DataPersistFailedException e){
+                logger.error("Failed to persist order data: {}", orderDTO, e);
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }catch (Exception e){
-                e.printStackTrace();
+                logger.error("Unexpected error occurred while saving order: {}", orderDTO, e);
                 return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
             }
         }
